cce7598a7f04af4bf13794cf27a4e1a08b639231
apersomany <59863085+apersomany@users.noreply.github.com>
2020-06-05 15:46:27 +0900

    EPUB feature added
    It does not follow the specs strictly, so it may not work with some viewers

--- kpapi.py
+++ kpapi.py
@@ -1,9 +1,12 @@
 import requests
 import string
 import random
-import json
+import uuid
+import time
+import os
 
 class account:
+
     def __init__(self, email, password, proxies=None):
         self.session = requests.Session()
         self.deviceId = randomHashGen()
@@ -81,45 +84,8 @@ class account:
             proxies=proxies
         )
 
-    def refresh(self):
-        tokens = self.session.post(
-            "https://kauth.kakao.com/oauth/token",
-            headers = {
-                'referer' : 'https://kauth.kakao.com',
-                'content-type' : 'application/x-www-form-urlencoded'
-            },
-            data = {
-                'grant_type' : 'refresh_token',
-                'client_id' : '49bbb48c5fdb0199e5da1b89de359484',
-                'refresh_token' : self.refresh_token
-            }
-        ).json()
-        
-        self.access_token = tokens['access_token']
-        if 'refresh_token' in tokens:
-            self.refresh_token = tokens['refresh_token']
-
-        uid = self.session.get(
-            "https://kapi.kakao.com/v2/user/me",
-            headers = {
-                'authorization' : 'bearer ' + self.access_token
-            }
-        ).text.split('id":')[1].split(',')[0]
-
-        self.session.get(
-            "https://page.kakao.com/main",
-            params = {
-                'loginHash' : self.deviceId,
-                'uid' : uid, 'at' : self.access_token,
-                'rt' : self.refresh_token
-            },
-            headers = {
-                'referer' : 'https://page.kakao.com/main'
-            }
-        )
-
     def useTicket(self, seriesid, singleid):
-        return self.session.post(
+        request = self.session.post(
             "https://api2-page.kakao.com/api/v6/store/use/ticket",
             data = {
                 'seriesid' : seriesid,
@@ -127,30 +93,213 @@ class account:
                 'deviceId' : self.deviceId
             }
         ).json()
+        if(request['result_code'] == 0 or request['result_code'] == 306):
+            return True
+        else:
+            return False
 
-    def getSingle(self, singleid):
+    def getDownloadData(self, singleid):
         return self.session.post(
             "https://api2-page.kakao.com/api/v1/inven/get_download_data/web",
             data = {
-                'productId' : '53192467',
+                'productId' : singleid,
                 'deviceId' : self.deviceId
             }
         ).json()['downloadData']['members']['files']
-    
-    def getImg(self, url):
-        return self.session.get("https://page-edge.kakao.com/sdownload/resource?kid="+ url).content
 
-def jsonPrint(data):
-    print(json.dumps(data, indent=4, sort_keys=True))
-    
+    def getSingle(self, singleid, firstPage=1):
+        downloadData = self.getDownloadData(singleid)
+        single = []
+        for i in range(firstPage, len(downloadData)):
+            single.append(self.getImg(downloadData[i]['secureUrl']), downloadData[i]['target'])
+        return single
+
+    def getImg(self, url, target):
+        if(target == 'page-edge'): return self.session.get("https://page-edge.kakao.com/sdownload/resource?kid="+ url).content
+        if(target == 'dn-img'): return self.session.get("https://dn-img-page.kakao.com/sdownload/resource?kid="+ url).content
+
+def createEpubStructure(title, author="aperso", coverimg=None, cachedir="cache", overwrite=True):
+    os.makedirs(cachedir, exist_ok=overwrite)
+    os.makedirs(cachedir + "/META-INF", exist_ok=overwrite)
+    os.makedirs(cachedir + "/EPUB", exist_ok=overwrite)
+    os.makedirs(cachedir + "/EPUB/singles", exist_ok=overwrite)
+    id = randomIDGen()
+    open(cachedir + "/mimetype", "w").write("application/epub+zip")
+    open(cachedir + "/META-INF/container.xml", "w").write(
+        '<?xml version="1.0" encoding="EUC-KR"?>'+
+        '<container version="1.0" xmlns="urn:oasis:names:tc:opendocument:xmlns:container">'+
+        '<rootfiles>'+
+        '<rootfile full-path="EPUB/package.opf" media-type="application/oebps-package+xml" />'+
+        '</rootfiles>'+
+        '</container>'
+    )
+
+    if(coverimg == None):
+        open(cachedir + "/EPUB/package.opf", "w").write(
+            '<?xml version="1.0" encoding="EUC-KR"?>' +
+            '<package version="3.0" unique-identifier="' +
+            id +
+            '" xmlns="http://www.idpf.org/2007/opf">' +
+            '<metadata xmlns:dc="http://purl.org/dc/elements/1.1/">' +
+            '<dc:identifier id="' +
+            id +
+            '">urn:uuid:' +
+            str(uuid.uuid1()) +
+            '</dc:identifier>' +
+            '<dc:title>' +
+            title +
+            '</dc:title>' +
+            '<dc:author>' +
+            author +
+            '</dc:author>' +
+            '</metadata>' +
+            '<manifest>' +
+            '<item id="nav" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav" />' +
+            '</manifest>' +
+            '<spine>' +
+            '</spine>' +
+            '</package>'
+        )
+    else:
+        open(cachedir + "/EPUB/package.opf", "w").write(
+            '<?xml version="1.0" encoding="EUC-KR"?>' +
+            '<package version="3.0" unique-identifier="' +
+            id +
+            '" xmlns="http://www.idpf.org/2007/opf">' +
+            '<metadata xmlns:dc="http://purl.org/dc/elements/1.1/">' +
+            '<dc:identifier id="' +
+            id +
+            '">urn:uuid:' +
+            str(uuid.uuid1()) +
+            '</dc:identifier>' +
+            '<dc:title>' +
+            title +
+            '</dc:title>' +
+            '<dc:author>' +
+            author +
+            '</dc:author>' +
+            '</metadata>' +
+            '<manifest>' +
+            '<item id="cover" href="cover.jpg" media-type="image/jpeg" properties="cover-image" />' +
+            '<item id="nav" href="nav.xhtml" media-type="application/xhtml+xml" properties="nav" />' +
+            '</manifest>' +
+            '<spine>' +
+            '<itemref idref="cover" />'
+            '</spine>' +
+            '</package>'
+        )
+        open(cachedir + "/EPUB/cover.jpg", "wb").write(coverimg)
+
+    open(cachedir + "/EPUB/nav.xhtml", "w").write(
+        '<?xml version="1.0" encoding="EUC-KR"?>'+
+        '<!DOCTYPE html>'+
+        '<html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">'+
+        '<head>'+
+        '<title>' + title + '</title>'+
+        '</head>'+
+        '<body>'+
+        '<nav epub:type="toc">'+
+        '<h1>"Table of Contents"</h1>'+
+        '<ol>'+
+        '</ol>'+
+        '</nav>'+
+        '</body>'+
+        '</html>'
+    )
+
+def addSingleToEpub(title, single, nameinepub, cachedir="cache"):
+    os.makedirs(cachedir + "/EPUB/singles/" + nameinepub, exist_ok=True)
+    htmlpart = ""
+    for i in range(len(single)):
+        open(cachedir + "/EPUB/singles/" + nameinepub + "/" + str(i) + ".jpg", "wb").write(single[i])
+        htmlpart = htmlpart + "<img src='" + str(i) + ".jpg' />"
+    open(cachedir + "/EPUB/singles/" + nameinepub + "/content.xhtml", "w").write(
+        '<?xml version="1.0" encoding="EUC-KR"?>'+
+        '<!DOCTYPE html>'+
+        '<html xmlns="http://www.w3.org/1999/xhtml">'+
+        '<head>'+
+        '<title>' + title + '</title>'+
+        '</head>'+
+        '<body>'+
+        htmlpart+
+        '</body>'+
+        '</html>'
+    )
+    package = open(cachedir + "/EPUB/package.opf", "r").read().split('</manifest>')
+    package[0] = package[0] + '<item id="' + nameinepub +  '" href="singles/' + nameinepub + '/content.xhtml" media-type="application/xhtml+xml" /></manifest>'
+    packagespine = package[1].split('</spine>')
+    packagespine[0] = packagespine[0] + '<itemref idref="' + nameinepub + '" /></spine>'
+    package[1] = "".join(str(i) for i in packagespine)
+    open(cachedir + "/EPUB/package.opf", "w").write("".join(str(i) for i in package))
+    nav = open(cachedir + "/EPUB/nav.xhtml", "r").read().split('</ol>')
+    nav[0] = nav[0] + '<li><a href="singles/' + nameinepub + '/content.xhtml">' + title + '</a></li></ol>'
+    open(cachedir + "/EPUB/nav.xhtml", "w").write("".join(str(i) for i in nav))
+
+
+def singleToEPUB(accounts, seriesid, firstPage = 1, firstSingle = 0):
+    seriesinfo = getSeriesInfo(seriesid)
+    seriessize = seriesinfo['home']['on_sale_count']
+    singles = getSingles(seriesid, "asc", seriessize)['singles']
+    createEpubStructure(seriesinfo['home']['title'], seriesinfo['home']['author_name'], getThumbnail(seriesinfo['home']['image_url']))
+    if(firstSingle == 0):
+        if(seriesinfo['has_free_single'] == True):
+            index = seriesinfo['free_single_count']
+        else:
+            index = 0
+        for i in range(index):
+            downloadData = getDownloadData(singles[i]['id'])
+            print("Downloading " + singles[i]['title'])
+            addSingleToEpub(str(singles[i]['title']), getSingle(singles[i]['id']), str(i))
+    else:
+        index = firstSingle
+
+    i = 0
+    while(index < len(singles) and i < len(accounts)):
+        if(accounts[i].useTicket(seriesid, singles[index]['id'])):
+            print("Downloading " + singles[index]['title'])
+            addSingleToEpub(str(singles[index]['title']), accounts[i].getSingle(singles[index]['id']), str(index))
+            index += 1
+            time.sleep(1)
+        else:
+            i += 1
+
+def updateEPUB(accounts, seriesid, firstPage = 1, firstSingle=0):
+    seriesinfo = getSeriesInfo(seriesid)
+    seriessize = seriesinfo['home']['on_sale_count']
+    singles = getSingles(seriesid, "asc", seriessize)['singles']
+    if(firstSingle == 0):
+        if(seriesinfo['has_free_single'] == True):
+            index = seriesinfo['free_single_count']
+        else:
+            index = 0
+        for i in range(index):
+            downloadData = getDownloadData(singles[i]['id'])
+            print("Downloading " + singles[i]['title'])
+            addSingleToEpub(str(singles[i]['title']), getSingle(singles[i]['id']), str(i))
+    else:
+        index = firstSingle
+
+    i = 0
+    while(index < len(singles) and i < len(accounts)):
+        if(accounts[i].useTicket(seriesid, singles[index]['id'])):
+            print("Downloading " + singles[index]['title'])
+            addSingleToEpub(str(singles[index]['title']), accounts[i].getSingle(singles[index]['id']), str(index))
+            index += 1
+            time.sleep(1)
+        else:
+            i += 1
+
 def randomHashGen():
     lettersAndDigits = string.ascii_lowercase + string.digits
     return ''.join((random.choice(lettersAndDigits) for i in range(32)))
 
-def getImg(self, url):
-    return requests.get("https://page-edge.kakao.com/sdownload/resource?kid="+ url).content
-def getThumbnail(url):
-    return requests.get("https://dn-img-page.kakao.com/download/resource?kid=" + url).content
+def randomIDGen():
+    lettersAndDigits = string.ascii_lowercase + string.digits
+    return ''.join((random.choice(lettersAndDigits) for i in range(6)))
+
+def getImg(url, target):
+    if (target == 'page-edge'): return requests.get("https://page-edge.kakao.com/sdownload/resource?kid=" + url).content
+    if (target == 'dn-img'): return requests.get("https://dn-img-page.kakao.com/sdownload/resource?kid=" + url).content
 
 def search(keyword):
     return requests.post(
@@ -160,14 +309,20 @@ def search(keyword):
         }
     ).json()["results"][0]["items"]
 
-def getSingle(self, singleid):
+def getDownloadData(singleid):
     return requests.post(
         "https://api2-page.kakao.com/api/v1/inven/get_download_data/web",
-        data = {
-        'productId' : '53192467',
-        'deviceId' : self.deviceId
+        data={
+            'productId': singleid
         }
     ).json()['downloadData']['members']['files']
+
+def getSingle(singleid, firstPage=1):
+    downloadData = getDownloadData(singleid)
+    single = []
+    for i in range(firstPage, len(downloadData)):
+        single.append(getImg(downloadData[i]['secureUrl']), downloadData[i]['target'])
+    return single
 
 def getSingles(seriesid, order="asc", pagesize="20", page="0"):
     return requests.post(
@@ -179,3 +334,12 @@ def getSingles(seriesid, order="asc", pagesize="20", page="0"):
             'direction' : order
         }
     ).json()
+
+def getSeriesInfo(seriesid):
+    return requests.post(
+        "https://api2-page.kakao.com/api/v5/store/home",
+        data = {
+            'seriesid' : seriesid
+        }
+    ).json()
+
